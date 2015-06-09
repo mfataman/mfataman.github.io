1d9f953140fe006794a0ec735e9f0371995d1ed6
mfataman <mfataman@yahoo.com>
2015-06-08 22:14:14 -0400

    fixed some issues

--- php/ajax-response.php
+++ php/ajax-response.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-type: application/json");
-echo json_encode( array( 'msg' => 'Hi! This is a message from the server' ));
+echo json_encode(array( 'msg' => 'Hi! This is a message from the server' ));
 die();
 
 ?>
